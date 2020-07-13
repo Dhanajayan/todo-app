0b70beaac9669591d1da1be73518fae3a016cd96
Gowthaman <gowthaman1129@gmail.com>
2020-07-13 19:37:46 +0530

    ORM controller function added

--- app/routes/todo.routes.js
+++ app/routes/todo.routes.js
@@ -3,14 +3,23 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
+  // Create a new Todo
   router.post("/", todo.create);
 
-  // Retrieve all todo
+  // Retrieve all Todo
   router.get("/", todo.findAll);
 
-  // Delete a Tutorial with id
+  // Retrieve a single Todo with id
+  router.get("/:id", todo.findOne);
+
+  // Update a Todo
+  router.put("/:id", todo.update);
+
+  // Delete a Todo with id
   router.delete("/:id", todo.delete);
+
+  // Delete all Todo
+  router.delete("/", todo.deleteAll);
 
 
   app.use('/api/todo', router);
